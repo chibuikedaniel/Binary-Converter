260627421364e2041b0085fa9b9234e6ba932095
dracuzi <danielchibuike77@gmail.com>
2024-10-12 20:49:12 +0100

    added time out for the animation and the app is now completed

--- script.js
+++ script.js
@@ -22,11 +22,17 @@ const animationContainer = document.getElementById("animation-container");
 const animationData = [
     {
         inputVal: 5,
-        addElDelay: 1000
+        addElDelay: 1000,
+        msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+        showMsgDelay: 15000,
+        removeElDelay: 20000
     },
     {
         inputVal: 2,
-        addElDelay: 1500
+        addElDelay: 1500,
+        msg: "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+        showMsgDelay: 10000,
+        removeElDelay: 15000
     },
     {
         inputVal: 1,
@@ -99,7 +105,16 @@ const showAnimation = () => {
                 <p id="${obj.inputVal}" class="animation-frame">decimalToBinary(${obj.inputVal})</p>
             `
         }, obj.addElDelay)
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).textContent = obj.msg;
+        }, obj.showMsgDelay)
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove();
+        }, obj.removeElDelay)
     })
+    setTimeout(() => {
+        result.textContent = decimalToBinary(5)
+    }, 20000)
 };
 
 const checkUserInput = () => {
